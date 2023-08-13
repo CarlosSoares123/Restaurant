c64aa643fa0c284409d0594a0bcab64a14cf1aae
Carlos Soares <142123548+CarlosSoaresCk@users.noreply.github.com>
2023-08-13 02:19:20 +0100

    update 0

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Category } from "./Components/category/category.jsx"
 import { Deals } from "./Components/deals/deals.jsx"
 import { Header } from "./Components/header/header.jsx"
 import { Hero } from './Components/hero/hero.jsx'
-import { Map } from "./Components/map/Map.jsx"
+import { Map } from "./Components/map/map.jsx"
 import { Restaurant } from "./Components/restauraunt/restaurant.jsx"
 import { Styled } from "./globalStyle.js"
 
